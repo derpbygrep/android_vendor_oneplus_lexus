a652ffb8f3c1a59659d513c7eafb3568ca0dc909
grep <grepfox@tutamail.com>
2025-09-15 22:39:28 +0530

    lexus: Initial Import
    Change-Id: Ibbb347c1b37521157b76ed19044c01f99cc16f8e

---
+++ Android.mk
@@ -0,0 +1,34 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lexus)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,bf44d5eb256cdca8c9545cd6fe1f1afcb5917a72)
+$(call add-radio-file-sha1-checked,radio/aop.img,964c09df6d598ea4b53e86a8b3b358b884678a04)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,5a32e8bd238adf486c18679ca79d1af8bb12747c)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,90e707bb8582ff441a89196271665f3b09803705)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,e6544b9b9b8cad32d46a1285dbde889ff805309c)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,ddd24ec0ba7cf002cb1f576f777141e464882a14)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,7ef7ae7f27507078a0bb6c78f73f3beb58cbcd2f)
+$(call add-radio-file-sha1-checked,radio/dsp.img,7e81146e07bb4120616a084cc932dc4836420d29)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,8c0509987affae47a8955a031ecb2b7ecc53eedc)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,d9c7ef76b2a9ba1ea270623b417e45a98a968271)
+$(call add-radio-file-sha1-checked,radio/hyp.img,e8bb707c36cec64e46361b9db1649da45c14d1fa)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,856e31615b96ce68c3fc11bfc1911750bd147a0c)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,9cbcf853cfde8ae71754b63b617ea7247a310b52)
+$(call add-radio-file-sha1-checked,radio/modem.img,c0c775d886dd422a296f060612ceb5a6a4451590)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,d4cd54f828dbfb4fcdb4c81fd6f1900da0e89287)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,559137cb729d6e4f28f03bd806fa97c72a5a3726)
+$(call add-radio-file-sha1-checked,radio/shrm.img,e0a4117717616c57c02e25803d02055b1c62f1fc)
+$(call add-radio-file-sha1-checked,radio/splash.img,a62387b428702e55e659bfd3a5ce0c662fabf41a)
+$(call add-radio-file-sha1-checked,radio/tz.img,da82dc515257ae94f58b38029e347e9f2dc807b6)
+$(call add-radio-file-sha1-checked,radio/uefi.img,264ebd8b08733ff948529f2d767f5a9810997ba8)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,18eeaebf247ddfab96b476ed406bd3ec4819dce8)
+$(call add-radio-file-sha1-checked,radio/xbl.img,0cf9674116ae4a1eaee66e47c3685da4622afca2)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,2689a9cd97a2be9fc1efaef002f879a80afc5437)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,934638cde340a2e89d491730aec8c4e9a2d5d73a)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,29 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    engineering_cdt \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    oplus_sec \
+    qupfw \
+    shrm \
+    splash \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
